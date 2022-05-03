ec75c66126ca460b55dd99745920690bcd6abf4e
tcirino <thenrique34@gmail.com>
2022-05-03 20:51:36 -0300

    alteração formulario

--- cadongs.php
+++ cadongs.php
@@ -61,7 +61,8 @@
                     <input type="password" name="senhaOng" id="senhaOng" class="white-text" />
                 </div>
             </div>
-
+        </form>
+        <form class="teal">
             <div class="row">
                 <div class="col s12 m4 ">
                     <h5 class="white-text">Conta</h5>
@@ -107,23 +108,122 @@
                     <textarea name="" id="" cols="30" rows="10"></textarea>
                 </div>
         </form>
+    </div>
 
-        <form class="teal">
+    <form class="teal">
+        <div class="row">
+            <div class="col s12 m4">
+                <h5 class="white-text">Endereço</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m4">
+                <label>Estado</label>
+                <select name="" id="">
+                    <option value=""></option>
+                    <option>AC</option>
+                    <option>AL</option>
+                    <option>AP</option>
+                    <option>AM</option>
+                    <option>BA</option>
+                    <option>CE</option>
+                    <option>DF</option>
+                    <option>ES</option>
+                    <option>GO</option>
+                    <option>MA</option>
+                    <option>MT</option>
+                    <option>MS</option>
+                    <option>MG</option>
+                    <option>PA</option>
+                    <option>PB</option>
+                    <option>PR</option>
+                    <option>PE</option>
+                    <option>PI</option>
+                    <option>RJ</option>
+                    <option>RN</option>
+                    <option>RS</option>
+                    <option>RO</option>
+                    <option>RR</option>
+                    <option>SC</option>
+                    <option>SP</option>
+                    <option>SE</option>
+                    <option>TO</option>
+                </select>
+            </div>
             <div class="row">
                 <div class="col s12 m4">
-                    <h5 class="white-text">Endereço</h5>
+                    <label for="cep">Cep</label>
+                    <input type="text" name="cep" id="cep" />
+                </div>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <label for="telefone">Telefone</label>
+                        <input type="text" name="telefone" id="telefone" />
+                    </div>
+                    <div class="row">
+                        <div class="col s12 m4">
+                            <label for="logradouro">Logradouro (ex:rua,avenida)</label>
+                            <input type="text" name="logradouro" id="logradouro" />
+                        </div>
+
+                        <div class="row">
+                            <div class="col s12 m4">
+                                <label for="numero">N°</label>
+                                <input type="text" name="numero" id="numero" />
+                            </div>
+
+
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
 
 
-            
-        </form>
+
+    </form>
+    <form class="teal">
+        <div class="row">
+            <div class="col s12 m4">
+                <h5 class="white-text">Dados Para Doações</h5>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m4">
+                <label for="numero">Pix Para Doações</label>
+                <input type="text" name="pix" id="pix" />
+            </div>
+            <div class="col s12 m4">
+                <label for="tipo">Tipo</label>
+                <select name="tipo" id="tipo">
+                    <option value=""></option>
+                    <option>CPF</option>
+                    <option>CNPJ</option>
+                    <option>Numero do Celular</option>
+                    <option>Email</option>
+                    <option>QR CODE</option>
+
+                </select>
+            </div>
+            <div class="row">
+                <div class="col s12 m4">
+                    <label for="numeroconta">Numero da Conta Para Doações</label>
+                    <input type="text" name="numeroconta" id="numeroconta" />
+                </div>
+            </div>
+    </form>
+
+    <div class="envio">
+        <button>Enviar</button>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script type="text/javascript">
+        M.AutoInit();
+    </script>
 
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <script type="text/javascript">
-            M.AutoInit();
-        </script>
 </body>
+
 
 </html>
